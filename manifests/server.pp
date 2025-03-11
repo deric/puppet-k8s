3d67727c537001743e3e5f202eef8cbc2ca67134
Jan Trojak <trojakjan24@gmail.com>
2025-03-11 12:44:34 +0100

    Include k8s::node

--- manifests/server.pp
+++ manifests/server.pp
@@ -138,18 +138,8 @@ class k8s::server (
   }
 
   if $node_on_server {
-    $_dir = $k8s::server::tls::cert_path
-
     class { 'k8s::node':
-      ensure            => $ensure,
-      control_plane_url => "https://localhost:${api_port}",
-      node_auth         => 'cert',
-      proxy_auth        => 'cert',
-      ca_cert           => $ca_cert,
-      node_cert         => "${_dir}/node.pem",
-      node_key          => "${_dir}/node.key",
-      proxy_cert        => "${_dir}/kube-proxy.pem",
-      proxy_key         => "${_dir}/kube-proxy.key",
+      ensure => $ensure,
     }
   }
 }
